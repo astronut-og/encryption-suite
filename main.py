1acee36f00729e5e10373fa3ca022f91dc13405a
RiskierGriffin <tenenbaumallan@protonmail.com>
2019-10-27 13:25:35 -0400

    init commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import salsa20
 
 if __name__ == "__main__":
-    message = "Arifur thanks for reviewing this"
+    message = input("Put in message here: ")
     key = salsa20.gen_key()
     encrypted_message = salsa20.encrypt_plaintext(message, key)
     print(encrypted_message)
