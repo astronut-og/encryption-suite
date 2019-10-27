73c03dba023a492886b0784328c928ba00da8c81
Allan Tenenbaum <53152572+astronut-og@users.noreply.github.com>
2019-10-27 14:59:47 -0400

    Delete main.py
    Useless testing file

--- main.py
+++
@@ -1,9 +0,0 @@
-import salsa20
-
-if __name__ == "__main__":
-    message = input("Put in message here: ")
-    key = salsa20.gen_key()
-    encrypted_message = salsa20.encrypt_plaintext(message, key)
-    print(encrypted_message)
-    decrypted_message = salsa20.decrypt_message(encrypted_message, key)
-    print(decrypted_message)
